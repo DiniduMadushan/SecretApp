b5d5711b6256df9fb5d276e522be2810374917df
DiniduMadushan <dinidumadushan15576@gmail.com>
2024-07-29 14:57:29 +0530

    login

---
+++ app.js
@@ -0,0 +1,76 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB")
+.then(()=>{
+    console.log("Database connected successfully");
+}).catch(err => console.log("Database connection failed: " + err));
+
+//create the user schema
+
+const userSchema = {
+    email : String,
+    password: String
+};
+
+//create the user modal
+const User = new mongoose.model("User", userSchema);
+
+app.get("/home", function(req,res){
+    res.render("home");
+}); 
+
+app.get("/login", function(req,res){
+    res.render("login");
+});
+
+app.get("/register", function(req,res){
+    res.render("register");
+});
+
+//register a new user
+app.post("/register", function(req,res){
+    const newUser = new User({email:req.body.username, password: req.body.password});
+    newUser.save()
+    .then(result => {
+        if(result){
+            res.render("secrets");
+        }else{
+            res.send("Registration failed!")
+        }
+    })
+    .catch(err => {
+        res.send("Error occured: "+ err);
+    });
+});
+
+app.post("/login", function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}).then(result=>{
+        if(result){
+            if(result.password === password){
+                res.render("secrets");
+            }else{
+                console.log("useranme or password is incorrect");
+            }
+        }else{
+            console.log("useranme or password is incorrect");
+        }
+    }).catch(err => console.log("error occured: " + err));
+
+});
+
+app.listen(3000, function(){
+    console.log("server started on port 3000");
+})
